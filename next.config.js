0e049ccd267617848d5bfbe44926618f42c7f249
bck0593 <bck88884@gmail.com>
2025-06-07 13:53:19 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-
+require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_ENDPOINT: process.env.API_ENDPOINT,
-  },
-  output: 'export', // ← これを追加
-};
+    env: {
+        // Reference a variable that was defined in the .env file and make it available at Build Time
+        API_ENDPOINT: process.env.API_ENDPOINT,
+      },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
