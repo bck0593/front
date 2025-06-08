ec1a77e54dbcd2b793dc3ff802d41ba64e39b5e4
bck0593 <bck88884@gmail.com>
2025-06-08 13:40:49 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-require('dotenv').config();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
